b6876479911aa38d74767c0c1d8de056aee17e56
timmson <timmson666@mail.ru>
2019-08-28 00:22:21 +0300

    add gradle

---
+++ src/test/java/ru/timmson/FirstTest.java
@@ -0,0 +1,14 @@
+package ru.timmson;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FirstTest {
+
+    @Test
+    public void shouldNotFail() {
+        assertTrue(true);
+    }
+
+}
