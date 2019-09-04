20a33ef5779cd8a2ee1ab820ab871dfe901ac86a
timmson <timmson666@mail.ru>
2019-09-04 23:47:55 +0300

    Step 3

--- src/main/java/ru/timmson/Discount.java
+++ src/main/java/ru/timmson/Discount.java
@@ -3,6 +3,14 @@ package ru.timmson;
 class Discount {
 
     public static int get(int amount) {
-        return (amount < 1000) ? 0 : 3;
+        int discount = 0;
+        if (amount >= 1000) {
+            if (amount < 5000) {
+                discount = 3;
+            } else if (amount < 7000) {
+                discount = 5;
+            }
+        }
+        return discount;
     }
 }
