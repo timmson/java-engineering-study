7a13693a7c535c3322baad1e0b648e0f2b45db65
timmson <timmson666@mail.ru>
2020-04-06 10:18:22 +0300

    Add Undertow

--- src/main/java/ru/timmson/load/DoSomething.java
+++ src/main/java/ru/timmson/load/DoSomething.java
@@ -1,12 +1,46 @@
 package ru.timmson.load;
 
-public class DoSomething {
+import io.undertow.Undertow;
+import io.undertow.util.Headers;
 
-    public void doSomething() {
-/*        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
+import java.io.IOException;
+import java.util.Scanner;
+
+import static java.util.concurrent.CompletableFuture.runAsync;
+
+public strictfp class DoSomething {
+
+    public static void main(String[] args) throws IOException {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(exchange -> {
+                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                    final var response = "OK";
+                    exchange.getResponseSender().send(response);
+                    souf("Response >> %s %s", response, Thread.currentThread().getName());
+                }).build();
+
+        server.start();
+
+        while (new Scanner(System.in).hasNextLine()) {
+
+        }
+
+        server.stop();
     }
+
+    private static void souf(final String message, final Object... args) {
+        runAsync(() -> {
+                    final var threadName = Thread.currentThread().getName();
+                    Object[] localArgs = new Object[]{threadName};
+                    if (args != null) {
+                        localArgs = new Object[args.length + 1];
+                        localArgs[0] = threadName;
+                        System.arraycopy(args, 0, localArgs, 1, args.length);
+                    }
+                    System.out.printf("[%s] " + message + "\r\n", localArgs);
+                }
+        );
+    }
+
 }
