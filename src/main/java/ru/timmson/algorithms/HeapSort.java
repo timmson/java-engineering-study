08a9205b61a72db330588393ab9ecd196e94e452
timmson <timmson666@mail.ru>
2020-04-14 00:13:51 +0300

    Add QuickSort

---
+++ src/main/java/ru/timmson/algorithms/HeapSort.java
@@ -0,0 +1,70 @@
+package ru.timmson.algorithms;
+
+import static java.lang.System.arraycopy;
+
+public class HeapSort extends ArraySort {
+
+    private static final HeapSort INSTANCE = new HeapSort();
+
+    public static HeapSort getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public void sort(int[] array) {
+        int i = 0;
+        final var rootNode = new Node(array[i++]);
+
+        for (; i < array.length; i++) {
+            rootNode.add(array[i]);
+        }
+
+        copy(rootNode.getValues(), array);
+    }
+
+    private static class Node {
+        private final int value;
+        private Node left;
+        private Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public void add(int newValue) {
+            if (newValue <= this.value) {
+                if (left != null) {
+                    left.add(newValue);
+                } else {
+                    this.left = new Node(newValue);
+                }
+            } else {
+                if (right != null) {
+                    right.add(newValue);
+                } else {
+                    this.right = new Node(newValue);
+                }
+            }
+        }
+
+        public int[] getValues() {
+            int[] leftArr = (left != null) ? left.getValues() : new int[0];
+            int[] rightArr = (right != null) ? right.getValues() : new int[0];
+            return concatenate(concatenate(leftArr, value), rightArr);
+        }
+
+        protected int[] concatenate(int[] array, int... values) {
+            int[] tmp = new int[array.length + values.length];
+            arraycopy(array, 0, tmp, 0, array.length);
+            arraycopy(values, 0, tmp, array.length, values.length);
+            return tmp;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    '}';
+        }
+    }
+}
---
+++ src/test/java/ru/timmson/algorithms/HeapSortShould.java
@@ -0,0 +1,60 @@
+package ru.timmson.algorithms;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class HeapSortShould {
+
+    final private HeapSort algorithm = HeapSort.getInstance();
+
+    @Test
+    void sortIntArrayOfOneNumbers() {
+        var n = new int[]{1};
+
+        algorithm.sort(n);
+
+        assertArrayEquals(new int[]{1}, n);
+    }
+
+    @Test
+        void sortIntArrayOfTwoNumbers() {
+        var n = new int[]{2, 1};
+
+        algorithm.sort(n);
+
+        assertArrayEquals(new int[]{1, 2}, n);
+    }
+
+    @Test
+    void sortIntArrayOfThreeNumbers() {
+        var n = new int[]{2, 3, 1};
+
+        algorithm.sort(n);
+
+        assertArrayEquals(new int[]{1, 2, 3}, n);
+    }
+
+    @Test
+    void sortIntArrayOfFiveNumbers() {
+        var n = new int[]{2, 3, 1, 2, 1};
+
+        algorithm.sort(n);
+
+        assertArrayEquals(new int[]{1, 1, 2, 2, 3}, n, IntStream.of(n).boxed().collect(toList()).toString());
+    }
+
+    @Test
+    void sortIntArrayOfTwentyNumbers() {
+        var n = new int[]{35, 30, 45, 26, 3, 35, 1, 45, 35, 35, 1, 27, 29, 22, 11, 17, 13, 21, 18, 7};
+
+        algorithm.sort(n);
+
+        assertArrayEquals(new int[]{1, 1, 3, 7, 11, 13, 17, 18, 21, 22, 26, 27, 29, 30, 35, 35, 35, 35, 45, 45}, n, IntStream.of(n).boxed().collect(toList()).toString());
+    }
+
+}
